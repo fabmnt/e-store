10fe7132d7b094f0afa0037418b7eb5f666fb687
Fabián Montoya <fabianmontoya2802@gmail.com>
2025-08-17 16:17:44 -0600

    Agregar la dependencia @radix-ui/react-aspect-ratio y actualizar el componente Collections para utilizar AspectRatio en las imágenes, mejorando la presentación visual.

--- src/collections/components/collections.tsx
+++ src/collections/components/collections.tsx
@@ -3,12 +3,13 @@
 import Image from 'next/image';
 import { Container } from '@/components/container';
 import { TextEffectPerLine } from '@/components/text-effect-per-line';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 
 export function Collections() {
   return (
     <section className="min-h-[500px] bg-orange-100">
       <Container className="space-y-4 py-8 2xl:space-y-8">
-        <div className="text-center text-8xl uppercase tracking-tight">
+        <div className="min-h-[200px] text-center text-8xl uppercase tracking-tight">
           <TextEffectPerLine
             text={`Explora nuestras
           Colecciones`}
@@ -16,21 +17,36 @@ export function Collections() {
         </div>
         <div className="flex items-center justify-center p-10">
           <div className="grid grid-cols-2 gap-6">
-            <Image
-              alt="Aretes"
-              className="row-span-2 h-full object-cover object-center"
-              height={600}
-              src="/aretes2.jpg"
-              width={600}
-            />
-            <Image
-              alt="Cadena"
-              className="max-w-[300px] object-cover object-center"
-              height={300}
-              src="/cadena1.jpg"
-              width={300}
-            />
-            <Image alt="Aretes" height={300} src="/aretes3.jpg" width={300} />
+            <div className="row-span-2">
+              <AspectRatio ratio={1 / 1}>
+                <Image
+                  alt="Aretes"
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  src="/aretes2.jpg"
+                />
+              </AspectRatio>
+            </div>
+            <div>
+              <AspectRatio ratio={1 / 1}>
+                <Image
+                  alt="Cadena"
+                  className="aspect-square h-full object-cover object-center"
+                  fill
+                  src="/cadena1.jpg"
+                />
+              </AspectRatio>
+            </div>
+            <div>
+              <AspectRatio ratio={1 / 1}>
+                <Image
+                  alt="Aretes"
+                  className="aspect-square h-full object-cover object-center"
+                  fill
+                  src="/aretes3.jpg"
+                />
+              </AspectRatio>
+            </div>
           </div>
         </div>
       </Container>
